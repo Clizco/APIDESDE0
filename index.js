d7cf661638fd4d4226f3abf5420359767da81d99
Clizco <gonzalezad06@gmail.com>
2023-06-19 16:43:42 -0500

    Create index.js
    Home de la api

---
+++ index.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+import express  from "express";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import accountRouter from "./routes/account.js";
+
+dotenv.config()
+
+const PORT = process.env.PORT || 1600;
+const expressApp = express()
+
+expressApp.use(cookieParser());
+expressApp.use(express.text());
+expressApp.use(express.json());
+
+expressApp.use("/account", accountRouter);
+
+
+const bootsrap = async () => {
+    await mongoose.connect(process.env.MONGODB_URL);
+   
+     expressApp.listen(PORT, () => 
+       console.log(`Servidor levantado en puerto ${PORT}`)
+     );
+   
+   }
+   
+   bootsrap();
