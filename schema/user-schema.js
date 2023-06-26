054f8c040bb39bcdb5415a4c602f4f27bc4a857a
Clizco <gonzalezad06@gmail.com>
2023-06-26 16:19:20 -0500

    Update user-schema.js

--- schema/user-schema.js
+++ schema/user-schema.js
@@ -1,12 +1,33 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
-    _id: String,
-    firstname: String,
-    lastname: String,
-    email: String,
-    born: String,
-    phone: String
+    _id: {
+        type: String,
+        require: [true],
+        unique: [true]
+    },
+    firstname: {
+        type: String,
+        require: [true]
+    },
+    lastname:{
+        type: String,
+        require: [true]
+    },
+    email: {
+        type: String,
+        require: [true],
+        unique: true,
+        },
+    born: {
+        type: String,
+        require: [true]
+    },
+    phone: {
+        type: String,
+        require: [true],
+        unique: [true]
+    },
 })
 
 const userModel = mongoose.model('User', userSchema)
