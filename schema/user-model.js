258f1fa8b74a9b6d5f24b6769f5814fee0924317
Clizco <gonzalezad06@gmail.com>
2023-07-27 12:27:40 -0500

    Create user-model.js

---
+++ schema/user-model.js
@@ -0,0 +1,16 @@
+import mongoose, { model } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    created:     { type: Date,   required: [true], trim: true, default: new Date() },
+    firstname:   { type: String, required: [true], trim: true },
+    lastname:    { type: String, required: [true], trim: true },
+    email:       { type: String, required: [true], trim: true, unique: true,},
+    dateofbirth :{ type: Date,   required: [true], trim: true, default: new Date().toISOString() },
+    phone:       { type: String, required: [true], trim: true, minlength: 8, maxlenght: 8,  unique: [true] },
+    modified:    { type: Date,   required: [true], trim: true, default: new Date() }
+});
+
+const userModel = model('User', userSchema)
+
+
+export default userModel; 
