e92ccdd8ed914377f8af1cdb3e66c7526de18c7f
Clizco <gonzalezad06@gmail.com>
2023-07-27 12:27:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express  from "express";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import accountRouter from "./routes/account.js";
-import userModel from "./schema/user-schema.js";
+import userModel from "./schema/user-model.js";
 
 dotenv.config()
 
