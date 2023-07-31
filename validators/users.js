a63a91c9348f187f33b3a87fbbcc3a4d36939a03
Clizco <86128395+Clizco@users.noreply.github.com>
2023-07-30 20:45:44 -0500

    Update users.js

--- validators/users.js
+++ validators/users.js
@@ -3,7 +3,7 @@ import { validateResult } from '../helpers/validateHelpers.js';
 
 const validateCreate = [
     check('firstname').not().isEmpty().withMessage('please fill this camp').exists().withMessage ('Firstname is required'),
-    check('lastname').not().isEmpty().withMessage('please fill this camp').exists().withMessage ('Lastname is required'),
+    check('lastname').not().isEmpty().withMessage('please fill this camp').exists().withMessage ('Lastname is required').isLength({ min: 5, max: 20}).withMessage('Please enter minimum 8 characters or maximun 20 characters  '),
     check('phone').not().isEmpty().withMessage('please fill this camp').exists().isNumeric().withMessage('Please only enter numbers').isLength({ min: 8, max: 8 }).withMessage('Please enter minimun 8 characters or maximun 8 characters'),
     check('dateofbirth').not().isEmpty().withMessage('please fill this camp').toDate().optional({ checkFalsy: true}).withMessage('Please enter valid dateofbirth'),
     check('email').exists().isEmail().withMessage('Please enter valid email'),
