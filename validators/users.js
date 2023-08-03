0f06d388b8c550bc521f2082b5573fbbf94bd527
Clizco <gonzalezad06@gmail.com>
2023-08-03 16:53:32 -0500

    Update users.js

--- validators/users.js
+++ validators/users.js
@@ -2,27 +2,15 @@ import { check } from 'express-validator';
 import { validateResult } from '../helpers/validateHelpers.js';
 
 const validateCreate = [
-    check('firstname')
-        .exists()
-        .not()
-        .isEmpty(),
-    check('lastname')
-        .exists()
-        .not()
-        .isEmpty(),
-    check('phone')
-        .exists()
-        .isNumeric(),
-    check('dateofbirth')
-        .exists()
-        .isNumeric(),
-    check('email')
-        .exists()
-        .isEmail(),
-        (req, res, next) => {
-            validateResult(req, res, next)
+    check('firstname').not().isEmpty().withMessage('please fill this camp').exists().withMessage ('Firstname is required'),
+    check('lastname').not().isEmpty().withMessage('please fill this camp').exists().withMessage ('Lastname is required'),
+    check('phone').not().isEmpty().withMessage('please fill this camp').exists().isNumeric().withMessage('Please only enter numbers').isLength({ min: 8, max: 8 }).withMessage('Please enter minimun 8 characters or maximun 8 characters'),
+    check('dateofbirth').not().isEmpty().withMessage('please fill this camp').toDate().optional({ checkFalsy: true}).withMessage('Please enter valid dateofbirth'),
+    check('email').exists().isEmail().withMessage('Please enter valid email'),
+            (req, res, next) => {
+        validateResult(req, res, next);
 
-        }
+    }
 
 ]
 
