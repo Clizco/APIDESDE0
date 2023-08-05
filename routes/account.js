88e3e6c0b5b315ea4409e0ffcdd620fd3a47533b
Clizco <gonzalezad06@gmail.com>
2023-08-04 22:35:22 -0500

    Update account.js

--- routes/account.js
+++ routes/account.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import userModel from "../schema/user-model.js";
 import { validateCreate } from '../validators/users.js' 
+import { validateUpdate } from "../validators/update.js";
 
 const accountRouter = Router()
 
@@ -16,7 +17,7 @@ accountRouter.get("/:email", async (req, res) => {
     const { email } = req.params ;
     const user = await userModel.findOne({email}).exec();
   
-    if (!user) return res.status(404).send();
+    if (!user) return res.status(404).send("");
   
     return res.send(user);
 });
@@ -28,28 +29,39 @@ accountRouter.post("/register/", validateCreate, async (req, res) => {
  
  
     const newUser = new userModel({ firstname, lastname, email, dateofbirth, phone});
- 
+    
     const userEmail = await userModel.findOne({email}).exec();
     const userPhone = await userModel.findOne({phone}).exec(); 
-
-    if(userEmail)
-        return res.status(409).send("Este email ya esta registrado, por favor utilice otro correo electronico.");
-
-    if(userPhone)   
-        return res.status(409).send("Este teléfono ya esta siendo usado, por favor utilice otro numero telefónico.");
-        
    
+    if(userEmail){
+        res.status(409).send({
+            error: "Email is already registered",
+        })
+     return
+    }
+
+        
+
+    if(userPhone){
+        res.status(409).send({
+            error: "Phone is already registered"
+        })
+    return
+    }
+    res.send({
+
+    });
+
+    
+        
     await newUser.save();
     
-    if (newUser)
-     return res.status(200).send("Se registro el usuario de manera exitosa!!")
-   
     
 
 });
 
 //Update users details
-accountRouter.patch('/update/:id', async (req, res) => {
+accountRouter.patch('/update/:id', validateUpdate, async (req, res) => {
     const id  = req.params.id; 
     const newUserData  = req.body; 	
     const user = await userModel.findOneAndUpdate({ _id: id }, {$set: newUserData});
@@ -64,13 +76,11 @@ accountRouter.patch('/update/:id', async (req, res) => {
 //Delete users
 accountRouter.delete("/delete/:email", async (req, res) => {
     const { email } = req.params;
-    const user = await userModel.findOne(email).exec();
+    const user = await userModel.findOneAndDelete(email).exec();
   
     if (!user) return res.status(404).send();
   
-    user.remove()
-  
-    return res.send();
+    return res.status(200).send('El usuario fue eliminado correctamente');
   
   });
   
