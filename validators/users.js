801d5030a5e3ce5ad5cae8497391966509f569bd
Clizco <gonzalezad06@gmail.com>
2023-08-04 22:35:26 -0500

    Update users.js

--- validators/users.js
+++ validators/users.js
@@ -2,18 +2,53 @@ import { check } from 'express-validator';
 import { validateResult } from '../helpers/validateHelpers.js';
 
 const validateCreate = [
-    check('firstname').not().isEmpty().withMessage('please fill this camp').exists().withMessage ('Firstname is required'),
 
-    check('lastname').not().isEmpty().withMessage('please fill this camp').exists().withMessage ('Lastname is required'),
+    // name validation
+    check('firstname')
+    .not()
+    .isEmpty()
+    .withMessage('please fill this camp')
+    .exists(),
 
-    check('lastname').not().isEmpty().withMessage('please fill this camp').exists().withMessage ('Lastname is required').isLength({ min: 5, max: 20}).withMessage('Please enter minimum 8 characters or maximun 20 characters  '),
-    check('phone').not().isEmpty().withMessage('please fill this camp').exists().isNumeric().withMessage('Please only enter numbers').isLength({ min: 8, max: 8 }).withMessage('Please enter minimun 8 characters or maximun 8 characters'),
-    check('dateofbirth').not().isEmpty().withMessage('please fill this camp').toDate().optional({ checkFalsy: true}).withMessage('Please enter valid dateofbirth'),
-    check('email').exists().isEmail().withMessage('Please enter valid email'),
+    // lastname validation
+    check('lastname')
+    .not()
+    .isEmpty()
+    .withMessage('please fill this camp')
+    .exists()
+    .withMessage ('Lastname is required')
+    .isLength({ min: 5, max: 20})
+    .withMessage('Please enter minimum 8 characters or maximun 20 characters  '),
+
+    // phone validation
+    check('phone')
+    .not()
+    .isEmpty()
+    .withMessage('please fill this camp')
+    .exists().withMessage("el telefono ya esta registrado")
+    .isNumeric()
+    .withMessage('Please only enter numbers')
+    .isLength({ min: 8, max: 8 })
+    .withMessage('Please enter minimun 8 characters or maximun 8 characters'),
+
+    // dateofbirth validation
+    check('dateofbirth')
+    .not()
+    .isEmpty()
+    .withMessage('please fill this camp')
+    .toDate()
+    .optional({ checkFalsy: true})
+    .withMessage('Please enter valid dateofbirth'),
+
+    // email validation
+    check('email')
+    .exists()
+    .withMessage('El email ya se encuentra registrado')
+    .isEmail()
+    .withMessage('Please enter valid email'),
             (req, res, next) => {
         validateResult(req, res, next);
 
-            validateResult(req, res, next)
 
 
     }
