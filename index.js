322c9546aca139413af51bed1a53beab2c6f0145
Clizco <gonzalezad06@gmail.com>
2023-08-10 15:04:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express  from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import accountRouter from "./routes/account.js";
 import userModel from "./schema/user-model.js";
@@ -13,6 +14,7 @@ const expressApp = express()
 expressApp.use(cookieParser());
 expressApp.use(express.text());
 expressApp.use(express.json());
+expressApp.use(cors());
 
 expressApp.use("/account", accountRouter);
 
