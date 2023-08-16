58bb0bf58f9820197af48814c332b4e9b1642c82
Clizco <gonzalezad06@gmail.com>
2023-08-16 09:21:17 -0500

    Update user-model.js

--- schema/user-model.js
+++ schema/user-model.js
@@ -4,7 +4,8 @@ const userSchema = new mongoose.Schema({
     created:     { type: Date,   required: [true], trim: true, default: new Date() },
     firstname:   { type: String, required: [true], trim: true },
     lastname:    { type: String, required: [true], trim: true },
-    email:       { type: String, required: [true], trim: true, unique: true,},
+    email:       { type: String, required: [true], trim: true, unique: true },
+    password:    { type: String, required: [true], trim: true, unique: true },
     dateofbirth :{ type: Date,   required: [true], trim: true, default: new Date().toISOString() },
     phone:       { type: String, required: [true], trim: true, minlength: 8, maxlenght: 8,  unique: [true] },
     modified:    { type: Date,   required: [true], trim: true, default: new Date() }
