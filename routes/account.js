5ff4665535b9bda09e40fc1ebd05877e6fec21a1
Clizco <gonzalezad06@gmail.com>
2023-08-22 20:20:20 -0500

    Update account.js

--- routes/account.js
+++ routes/account.js
@@ -1,7 +1,10 @@
-import { Router } from "express";
+import { Router, json, response } from "express";
 import userModel from "../schema/user-model.js";
 import { validateCreate } from '../validators/users.js' 
 import { validateUpdate } from "../validators/update.js";
+import jwt from "jsonwebtoken"
+import config from "../config.js";
+
 
 const accountRouter = Router()
 
@@ -24,39 +27,58 @@ accountRouter.get("/:email", async (req, res) => {
 
 //get all users details
 accountRouter.get("/users/all", async (req, res) => {
-    const user = await userModel.find({}).exec();
+    const user = await userModel.find({}, {password: 0}).exec();
 
     return res.status(200).json(user)
+}) 
+
+//get me user details
+accountRouter.get("/users/me", async (req, res) => {
+
+    const token = req.headers['x-access-token'];
+    if(!token) {
+        return res.status(401).json({
+            auth: false,
+            message: 'No token provided'
+        })
+    }
+
+    const decoded = jwt.verify(token, config.secret);
+    const user = await userModel.findById(decoded.id, {password: 0})
+    if(!user) {
+        return res.status(404).send('No user found')
+    }
+
+    res.json(user)
 })
 
 
 //add user details
-accountRouter.post("/register/", validateCreate, async (req, res) => {
-    const {email, firstname, lastname, dateofbirth, phone  } = req.body;
+accountRouter.post("/signup/", validateCreate, async (req, res) => {
+    const {email, firstname, lastname, password, dateofbirth, phone  } = req.body;
  
- console.log(JSON.stringify(req.body))
-
-    const newUser = new userModel({ firstname, lastname, email, dateofbirth, phone});
+ 
+    const newUser = new userModel({ firstname, lastname, email, password,  dateofbirth, phone});
     
     const userEmail = await userModel.findOne({email}).exec();
     const userPhone = await userModel.findOne({phone}).exec(); 
-
+    newUser.password = await newUser.encryptPassword(newUser.password);
+    const token = jwt.sign({id: newUser._id}, config.secret, {
+        expiresIn: 60 * 60 * 24
+    })
+    
     if (!dateofbirth) {
         res.status(409).send({
             error: "Thats not a date.",
         })
      return
     }
-   
     if(userEmail){
         res.status(409).send({
             error: "Email is already registered",
         })
      return
     }
-
-        
-
     if(userPhone){
         res.status(409).send({
             error: "Phone is already registered"
@@ -64,12 +86,31 @@ accountRouter.post("/register/", validateCreate, async (req, res) => {
     return
     }
     
-        res.status(200).send({"info": "User Registered"})
+        res.json({auth: true, token})
     await newUser.save();
-    
-    
 
 });
+
+//signing user details
+accountRouter.post('/signin', async (req, res) => {
+    const {email, password} = req.body;
+    const user = await userModel.findOne({email: email});
+    if (!user) {
+        return res.status(404).send("The email doesn't exists")
+    }
+
+    const validPassword = await user.validatePassword(password);
+    if(!validPassword) {
+        return res.status(401).json({auth: false, token: null})
+
+    }
+
+    const token = jwt.sign({id: user._id}, config.secret, {
+        expiresIn: 60 * 60 * 24
+    })
+    res.json({auth: true, token})
+  
+})
 
 //update users details
 accountRouter.patch('/update/:id', validateUpdate, async (req, res) => {
